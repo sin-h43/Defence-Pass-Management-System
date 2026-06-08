90246406613a73083c3c4ab2af0326cd76d8b83d
Sinchana K <sinchanakrishnamurthy07@gmail.com>
2026-06-08 20:10:37 +0530

    Added slidable sidebar, redesigned the metric cards

--- defence-portal/src/components/DashboardLayout.tsx
+++ defence-portal/src/components/DashboardLayout.tsx
@@ -1,10 +1,12 @@
 import {cn} from '../lib/utils'
 import React, {useState} from 'react';
-import { LayoutDashboard,Users, FileText, ShieldAlert, UserCheck, Settings, LogOut, ChevronRight,Search, Bell } from 'lucide-react';
+import { LayoutDashboard,Users, FileText, ShieldAlert, UserCheck, Settings, LogOut, ChevronRight,Search, Bell, PanelLeft,PanelRight } from 'lucide-react';
 
 
 export default function DashboardLayout({children}: {children: React.ReactNode}){
     const [activeTab, setActiveTab] = useState("Dashboard")
+    const [isPanelOpen, setIsPanelOpen] = useState(false)
+
     const navItems = [
         {name:"Dashboard", icon: LayoutDashboard},
         {name:"Visitor Management", icon: Users, hasSub:true},
@@ -17,18 +19,25 @@ export default function DashboardLayout({children}: {children: React.ReactNode})
     return(
         <div className='flex w-screen h-screen overflow-hidden'>
             {/*SideBar nav */}
-            <aside className='bg-gray-900 border-r border-gray-700 flex flex-col justify-start items-center gay-3'>
+            <aside className={cn('bg-gray-900 border-r border-gray-700 flex flex-col justify-between p-4 shrink-0 transition-all duration-300 ease-in-out gap-3',
+              isPanelOpen? 'w-64 items-stretch' : 'w-16 items-center px-2'
+            )}>
             <div>
                 {/*header logo box*/}
-             <div className="p-5 border-b border-slate-800/60 flex items-center gap-3">
+             <div className={cn("p-5 border-b border-slate-800/60 flex items-center gap-3",
+                !isPanelOpen && 'border-none bg-gray-800/40 justify-center'
+             )}>
             <div className="h-7 w-7 bg-amber-500 rounded flex items-center justify-center font-black text-black text-xs tracking-wider">
               DF
             </div>
-            <div>
+            {isPanelOpen && (
+              <div >
               <h1 className="font-bold text-xs tracking-wide text-white uppercase">Defence System</h1>
               <p className="text-[9px] text-amber-500 font-mono tracking-widest uppercase">Visitor Pass</p>
             </div>
+            )}
           </div>   
+
           <nav className='p-3 space-y-1 mt-4 text-white'>
             {navItems.map((item)=>{
                 const Icon = item.icon
@@ -38,14 +47,15 @@ export default function DashboardLayout({children}: {children: React.ReactNode})
                         key={item.name}
                         onClick={()=>setActiveTab(item.name)}
                         className={cn(
-                            "h-10 bg-gray-700 rounded border border-amber-500/50 flex items-center text-sm hover:bg-gray-800 cursor-pointer transition-colors w-full",
-              isActive ? 'px-3 justify-start' : 'justify-center p-0'
+                          "group w-full h-10 bg-gray-700 rounded border border-amber-500/50 flex items-center text-sm hover:bg-gray-800 cursor-pointer transition-colors",
+              isActive ? 'px-3 justify-between' : 'justify-center p-0',
+              !isPanelOpen && 'justify-center px-0'
               )}>
                 <div className="flex items-center gap-3">
                     <Icon className={cn("h-4 w-4", isActive ? "text-amber-500" : "text-slate-400 group-hover:text-slate-200")} />
-                    <span>{item.name}</span>
+                    {isPanelOpen && <span>{item.name}</span>}
                   </div>
-                  {item.hasSub && (
+                  {item.hasSub && isPanelOpen && (
                     <ChevronRight className="h-3 w-3 text-slate-500 group-hover:text-slate-300" />
                   )}
               </button>
@@ -59,21 +69,27 @@ export default function DashboardLayout({children}: {children: React.ReactNode})
             <div className="h-8 w-8 rounded-full bg-slate-800 border border-slate-700 flex items-center justify-center text-xs font-semibold text-white">
               SH
             </div>
-            <div className='items-down'>
+            {isPanelOpen && (
+              <div className='items-down'>
               <p className="text-xs font-medium text-slate-200">Sinchana K</p>
               <p className="text-[10px] text-slate-500 font-mono">Admin Officer</p>
             </div>
+            )}
           </div>
           <button className="text-slate-500  hover:text-rose-400 transition-colors">
             <LogOut className="h-4 w-4" />
           </button>
         </div>
         </aside>
-        <main className='flex flex-col items-center'>
+        <main className='flex flex-col flex-1 overflow-hidden'>
             {/*Top Header Bar */}
             <header className="h-16 bg-gray-900 border-b border-[#21262d] shrink-0 flex items-center justify-between px-6 z-10 gap-3">
-            <div className="flex items-center bg-slate-900 border border-slate-850 rounded-lg px-3 py-2">
-              <Search className="text-gray-500 mr-2 h-4 w-4" />
+              <div className='text-gray-400'
+                onClick={()=> setIsPanelOpen(!isPanelOpen)}>
+                {isPanelOpen ? <PanelLeft size={20} /> : <PanelRight size={20}/>}
+              </div>
+            <div className="flex items-center bg-slate-900 border border-slate-800 rounded-lg px-3 py-2 w-80">
+              <Search className="text-gray-500 mr-2 h-4 w-4 shrink-0" />
               <input
                 type="text"
                 placeholder="Search visitors, requests, ID..."
--- defence-portal/src/pages/hr/HrDashboard.tsx
+++ defence-portal/src/pages/hr/HrDashboard.tsx
@@ -1,54 +1,80 @@
 import {cn} from '../../lib/utils'
 import DashboardLayout from '@/components/DashboardLayout';
-import React, {useState} from 'react';
-import {Plus, ShieldCheck} from 'lucide-react'
+import {Users, UserX, UsersRound, UserCheck} from 'lucide-react'
 
 // Reusable card structure defined locally to keep things robust
 interface MetricCardProps {
   title: string
+  body: string
   value: string
   type: "pending" | "active" | "denied" | "total"
 }
-function MetricCard({title, value, type}: MetricCardProps){
+function MetricCard({title, value, body, type}: MetricCardProps){
+        const iconMap = {
+            pending: Users,
+            active: UserCheck,
+            denied: UserX,
+            total: UsersRound,
+        }
+        const iconColorMap = {
+            pending: "text-amber-500",
+            active: "text-emerald-500",
+            denied: "text-rose-500",
+            total: "text-blue-500",
+        }
 
+        const IconComponent = iconMap[type]
         return(
-             <div className='bg-[#0bf19] border border-gray-600/80 p-5 rounded-xl text-white bg-gray-800'>
-            <p>{title}</p>
-            <p className={cn(
-                "text-2xl front-bold mt-2",
-                type === 'pending' && "text-amber-500",
-                type === 'active' && "text-emerald-500",
-                type === 'denied' && "text-rose-500",
-                type === 'total' && "text-blue-500",
-            )}> {value}</p>
+            <div className='flex items-start justify-between group bg-gray-900 border border-gray-600/80 p-5 rounded-xl text-gray-200 font-semibold hover:bg-gray-800 '>
+            <div className='space-y-1'>
+                <p className={cn(
+                    type === 'pending' && "group-hover:text-amber-500 ",
+                    type === 'active' && "group-hover:text-emerald-500",
+                    type === 'denied' && "group-hover:text-rose-500",
+                    type === 'total' && "group-hover:text-blue-500",
+                )}
+                >{title}</p>
+                <p className="text-xs  text-gray-400 max-w-sm">{body}</p>
+                <p className={cn(
+                    "text-2xl font-semibold mt-2 ",
+                    type === 'pending' && "text-amber-500/70 ",
+                    type === 'active' && "text-emerald-500/70",
+                    type === 'denied' && "text-rose-500/70",
+                    type === 'total' && "text-blue-500/70",
+                )}> {value}</p>
+            </div>
+            <div className={cn(
+                "p-3 rounded-lg shrink-0",
+                    type === 'pending' && "bg-amber-500/20 ",
+                    type === 'active' && "bg-emerald-500/20",
+                    type === 'denied' && "bg-rose-500/20",
+                    type === 'total' && "bg-blue-500/20",
+            )}>
+                <IconComponent className={cn(
+                    "h-5 w-5",
+                    iconColorMap[type]
+                )} />
+            </div>
         </div>
         )
 }
 
 export default function HrDashboard(){
-    const [isPanelOpen, setIsPanelOpen] = useState(false)
-
     return(
         <DashboardLayout>
             <div className='space-y-6 '>
             {/*Head description Section */}
-                <div>
+                <div className='mb-3'>
                     <h2 className='text-xl font-bold text-white tracking-tight'>HR Visitor Control Panel</h2>
                     <p className='text-xs text-gray-400 mt-1'>Manage, approve, and track active military personnel and contractor gate entry requests.</p>
                 </div>
             </div>
-            
-            <button 
-                onClick={()=> setIsPanelOpen(true)}
-                className='flex items-center gap-2 bg-amber-500 hover:bg-amber text-black px-4 py-2 rounded-lg text-xs font-semibold transition-all shadow-lg shadow-amber-500/10 cursor-pointer'>
-                    <Plus className = "h-4 w-4 stroke-[3]" /> Create Pass Request
-            </button>
             {/*Metric Overview Row */}
         <div className="grid grid-cols-1 mt-2 md:grid-cols-4 gap-4">
-          <MetricCard title="Total Requests" value="218" type="total"  />          
-          <MetricCard title="Pending Approvals" value="12" type="pending" />
-          <MetricCard title="Active Passes" value="45" type="active" />
-          <MetricCard title="Denied Passes" value="02" type="denied" />
+          <MetricCard title="Total Requests"body='~' value="218" type="total"  />          
+          <MetricCard title="Pending Approvals" body='Awaiting approval' value="12" type="pending" />
+          <MetricCard title="Active Passes" body='Currently inside' value="45" type="active" />
+          <MetricCard title="Denied Passes" body='-5% from yday' value="02" type="denied" />
         </div>
         </DashboardLayout>
     )
